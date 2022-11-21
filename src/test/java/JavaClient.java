7a2623a79717b9f2f8c82e3a6b0e5b74a3ce6aa8
mpateriya-deloitte <mpateriya@deloitte.com>
2022-11-21 12:31:04 +0530

    Added Comments

--- src/test/java/JavaClient.java
+++ src/test/java/JavaClient.java
@@ -1,19 +1,26 @@
+//importing Required Libraries
+
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 import java.net.URL;
 import java.util.*;
 
+//Creating the Client Class
 public class JavaClient {
     public static void main (String [] args) {
 
         try {
+            //Initializing the Client Configuration
             XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+            //Setting the Host, port and URL of Server to Access
             config.setServerURL(new URL("http://localhost:8080/xmlrpc"));
             XmlRpcClient client = new XmlRpcClient();
             client.setConfig(config);
 
+            //Using Objects to Send via the Client to Server
             Object[] sum = new Object[]{2, 3};
+            //Executing the method
             int result = (int) client.execute("add", sum);
             System.out.println("The sum is: "+ result);
 
